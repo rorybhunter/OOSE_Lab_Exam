45136aa4a8af80450adc4f08a5fe33d60847f27d
“Rory <2655888H@student.gla.ac.uk>
2023-03-10 14:28:53 +0000

    created encounters and encounter factory

--- src/EncounterFactory.java
+++ src/EncounterFactory.java
@@ -1,4 +1,16 @@
-public class EncounterFactory
-{
-    cre
+public class EncounterFactory {
+    public static Encounter createEncounter(String type, Coordinates coordinates) {
+        switch (type) {
+            case "Flatlands":
+                return new Flatlands(type, coordinates);
+            case "Finish point":
+                return new FinishPoint(type, coordinates);
+            case "Wolf":
+                return new Wolf(type, coordinates);
+            case "Lake":
+                return new Lake(type, coordinates);
+            default:
+                throw new IllegalArgumentException("Invalid encounter type: " + type);
+        }
+    }
 }
--- src/Lake.java
+++ src/Lake.java
@@ -3,6 +3,6 @@ public class Lake extends Encounter{
     public Lake(String name, Coordinates coordinates) {
         super(name, coordinates);
         this.nearbyAlert = "there is a vast expanse of water.";
-        this.encounterDetails = "The water is icy cold, you take 10 damage!"
+        this.encounterDetails = "The water is icy cold, you take 10 damage!";
     }
 }
